fc24f071d3a3e1f0e4415ca9fe26136ce9a0a92e
Keith Vhurumuku <tawekith@stanford.edu>
2024-06-25 10:34:28 +0200

    "Revalidate"

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import { client, UrlFor } from "./lib/sanity";
 import Image from "next/image"
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+
+export const revalidate = 30;
+
 async function getData() {
   const query = `
   *[_type == 'post'] | order(_updatedAt desc) {
